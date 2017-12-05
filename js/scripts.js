1160b1b0fc7d58db35da3f6b3682e4ab2bf919a3
Kyle Tenzeldam and Natalia Telpukhova <natalia@>
2017-12-05 09:24:10 -0800

    change forEach to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   var flavors = ["chocolate", "vanilla", "coffee", "hazelnut"];
 
-  flavors.forEach(function(flavor) {
-    $("body").append("<p>" + flavor + "</p>");
-  });
+  // flavors.forEach(function(flavor) {
+  //   $("body").append("<p>" + flavor + "</p>");
+  // });
+
+  for (var index = 0; index < flavors.length; index += 1) {
+    $("body").append("<p>" + flavors[index] + "</p>");
+  }
+  
 });
